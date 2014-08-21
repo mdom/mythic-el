e2c7b0beb2708635e8c8259e3a9d94df709a3476
Mario Domgoergen <mario@domgoergen.com>
2014-08-21 17:04:21 +0200

    indent action block

--- mythic.el
+++ mythic.el
@@ -112,11 +112,31 @@
 ; focusArray[6] = new Array("5/Epic game: the Chaos Factor cannot fall below 3.  Any scene which would normally lower the Chaos below 3 leaves it unchanged."  "1/12/Thread escalates"  "13/16/Remote event"  "17/30/NPC action"  "31/42/Introduce an NPC"  "43/46/Move toward a thread"  "47/58/Move away from a thread"  "59/72/PC negative"  "73/80/PC positive"  "81/84/Ambiguous event"  "85/92/NPC negative"  "93/100/NPC positive")
 
 (setq Actions '(
-		"Attainment"  "Starting"  "Neglect"  "Fight"  "Recruit"  "Triumph"  "Violate"  "Oppose"  "Malice"  "Communicate"  "Persecute"  "Increase"  "Decrease"  "Abandon"  "Gratify"  "Inquire"  "Antagonise"  "Move"  "Waste"  "Truce"  
-		"Release"  "Befriend"  "Judge"  "Desert"  "Dominate"  "Procrastinate"  "Praise"  "Separate"  "Take"  "Break"  "Heal"  "Delay"  "Stop"  "Lie"  "Return"  "Imitate"  "Struggle"  "Inform"  "Bestow"  "Postpone"  
-		"Expose"  "Haggle"  "Imprison"  "Release"  "Celebrate"  "Develop"  "Travel"  "Block"  "Harm"  "Debase"  "Overindulge"  "Adjourn"  "Adversity"  "Kill"  "Disrupt"  "Usurp"  "Create"  "Betray"  "Agree"  "Abuse"  
-		"Oppress"  "Inspect"  "Ambush"  "Spy"  "Attach"  "Carry"  "Open"  "Carelessness"  "Ruin"  "Extravagance"  "Trick"  "Arrive"  "Propose"  "Divide"  "Refuse"  "Mistrust"  "Deceive"  "Cruelty"  "Intolerance"  "Trust"  
-		"Excitement"  "Activity"  "Assist"  "Care"  "Negligence"  "Passion"  "Work hard"  "Control"  "Attract"  "Failure"  "Pursue"  "Vengeance"  "Proceedings"  "Dispute"  "Punish"  "Guide"  "Transform"  "Overthrow"  "Oppress"  "Change"
+		"Attainment" "Starting"        "Neglect"     "Fight"
+		"Recruit"    "Triumph"         "Violate"     "Oppose"
+		"Malice"     "Communicate"     "Persecute"   "Increase"
+		"Decrease"   "Abandon"         "Gratify"     "Inquire"
+		"Antagonise" "Move"            "Waste"       "Truce"
+		"Release"    "Befriend"        "Judge"       "Desert"
+		"Dominate"    "Procrastinate"  "Praise"      "Separate"
+		"Take"        "Break"          "Heal"        "Delay"
+		"Stop"        "Lie"            "Return"      "Imitate"
+		"Struggle"    "Inform"         "Bestow"      "Postpone"
+		"Expose"      "Haggle"         "Imprison"    "Release"
+		"Celebrate"   "Develop"        "Travel"      "Block"
+		"Harm"        "Debase"         "Overindulge" "Adjourn"
+		"Adversity"   "Kill"           "Disrupt"     "Usurp"
+		"Create"      "Betray"         "Agree"       "Abuse"
+		"Oppress"     "Inspect"        "Ambush"      "Spy"
+		"Attach"      "Carry"          "Open"        "Carelessness"
+		"Ruin"        "Extravagance"   "Trick"       "Arrive"
+		"Propose"     "Divide"         "Refuse"      "Mistrust"
+		"Deceive"     "Cruelty"        "Intolerance" "Trust"
+		"Excitement"  "Activity"       "Assist"      "Care"
+		"Negligence"  "Passion"        "Work hard"   "Control"
+		"Attract"     "Failure"        "Pursue"      "Vengeance"
+		"Proceedings" "Dispute"        "Punish"      "Guide"
+		"Transform"   "Overthrow"      "Oppress"     "Change"
 		))
 
 (setq Subjects '(
