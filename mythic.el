3336571ff47fbff2cd7c0ca1c63d4fa3ca546e36
Mario Domgoergen <mario@domgoergen.com>
2014-08-25 18:28:09 +1000

    format answer to question to minibuffer

--- mythic.el
+++ mythic.el
@@ -100,9 +100,9 @@
 
 (defun mythic-odds-question (acting)
   (interactive)
-  (mythic-get-odds acting (mythic-chaos-level-rank mythic-chaos-level)))
-
 ; focusArray[1] = new Array("4/Horror: the game starts with the Chaos Factor set to 4.  Chaos can only increase not decrease.  When random events are generated results of 1 to 3 within the Chaos Factor are altered scenes.  Any higher numbers will be interrupts."  "1/10/Horror - PC"  "11/23/Horror - NPC"  "24/30/Remote Event"  "31/49/NPC action"  "50/52/Introduce an NPC"  "53/55/Move toward a thread"  "56/62/Move away from a thread"  "63/72/PC Negative"  "73/75/PC positive"  "76/82/Ambiguous event"  "83/97/NPC negative"  "98/100/NPC positive");
+  (mythic-format-answer
+   (mythic-get-odds acting (mythic-chaos-level-rank mythic-chaos-level))))
 
 ; focusArray[2] = new Array("5/Action adventure: Double rolls on the Fate chart always result in a random event whether they fall within the Chaos Facotr range or not.  Chaos cannot fall below 5.  Any scene which would normally lower the Chaos below 5 leaves it unchanged."  "1/16/Action!"  "17/24/Remote event"  "25/44/NPC action"  "45/52/Introduce an NPC"  "53/56/Move toward a thread"  "57/64/Move away from a thread"  "65/76/PC negative"  "77/80/PC positive"  "81/84/Ambiguous event"  "85/96/NPC negative"  "97/100/NPC positive")
 
