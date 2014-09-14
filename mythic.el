045d35a088c91c34ee6bb39d228c7609f87a8f36
Mario Domgoergen <mario@domgoergen.com>
2014-09-14 20:06:15 +1000

    add function mythic to load or create mythic files

--- mythic.el
+++ mythic.el
@@ -402,6 +402,20 @@ use and can either of the symbols odd or resisted."
   (interactive "sNPC: ")
   (mythic-add-list-element "NPCs" npc))
 
+(defun mythic (file)
+  (interactive "FFind adventure file: ")
+  (find-file file)
+  (if (not (file-exists-p (buffer-file-name)))
+      (let ((prompt "Scene: "))
+	(when (y-or-n-p "Random adventure setup? ")
+	  (setq prompt (format "Scene setup (%s): " (mythic-random-event))))
+	(insert (format "Scene: %s (Chaos: %d)\n\n\n"
+			(read-string prompt) mythic-chaos-level))
+	(goto-char (- (point) 3)))
+    (while (re-search-forward "^Scene: ")))
+  (narrow-to-page)
+  (mythic-mode))
+
 (provide 'mythic)
 
 ;; focusArray[1] = new Array("4/Horror: the game starts with the Chaos Factor set to 4.  Chaos can only increase not decrease.  When random events are generated results of 1 to 3 within the Chaos Factor are altered scenes.  Any higher numbers will be interrupts."  "1/10/Horror - PC"  "11/23/Horror - NPC"  "24/30/Remote Event"  "31/49/NPC action"  "50/52/Introduce an NPC"  "53/55/Move toward a thread"  "56/62/Move away from a thread"  "63/72/PC Negative"  "73/75/PC positive"  "76/82/Ambiguous event"  "83/97/NPC negative"  "98/100/NPC positive");
